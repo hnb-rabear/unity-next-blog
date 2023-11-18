3213bcd91d1ce147e56102d3d3fae5c7cc998ed2
RadBear <nbhung71711@gmail.com>
2023-11-18 23:35:23 +0700

    Personalize siteMetadata.js

--- layouts/AuthorLayout.tsx
+++ layouts/AuthorLayout.tsx
@@ -30,14 +30,18 @@ export default function AuthorLayout({ children, content }: Props) {
                 className="h-48 w-48 rounded-full"
               />
             )}
+            <p>{email}</p>
+            <p>{github}</p>
+            <p>{linkedin}</p>
+            <p>{twitter}</p>
             <h3 className="pb-2 pt-4 text-2xl font-bold leading-8 tracking-tight">{name}</h3>
             <div className="text-gray-500 dark:text-gray-400">{occupation}</div>
             <div className="text-gray-500 dark:text-gray-400">{company}</div>
             <div className="flex space-x-3 pt-6">
-              <SocialIcon kind="mail" href={`mailto:${email}`} />
-              <SocialIcon kind="github" href={github} />
-              <SocialIcon kind="linkedin" href={linkedin} />
-              <SocialIcon kind="twitter" href={twitter} />
+              {email && <SocialIcon kind="mail" href={`mailto:${email}`} />}
+              {github && <SocialIcon kind="github" href={github} />}
+              {linkedin && <SocialIcon kind="linkedin" href={linkedin} />}
+              {twitter && <SocialIcon kind="twitter" href={twitter} />}
             </div>
           </div>
           <div className="prose max-w-none pb-8 pt-8 dark:prose-invert xl:col-span-2">
