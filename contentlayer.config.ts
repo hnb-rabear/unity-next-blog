441ad116cf865eac3398d26d08c62543bdba27da
RadBear <nbhung71711@gmail.com>
2023-11-20 06:36:37 +0700

    Add my first blog

--- contentlayer.config.ts
+++ contentlayer.config.ts
@@ -124,6 +124,7 @@ export const Authors = defineDocumentType(() => ({
         linkedin: { type: 'string' },
         github: { type: 'string' },
         layout: { type: 'string' },
+        language: { type: 'string', default: 'en' },
     },
     computedFields,
 }));
